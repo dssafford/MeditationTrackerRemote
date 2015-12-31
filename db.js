1450cffbb2d178f0e49ff53879e105c570408ac0
Doug Safford <doug.safford@yahoo.com>
2015-12-31 12:16:10 -0600

    Deploy to CF worksgit add . db uri in db.js correct now, needs cleanup

--- db.js
+++ db.js
@@ -7,6 +7,8 @@ var mongoose = require('mongoose');
  //mongodb://<dbuser>:<dbpassword>@ds037175.mongolab.com:37175/CloudFoundry_l1hnqf0n_mmhir01l
 
 //uri: "mongodb://cloudfoundry-test_2p6otl8c_841b7q4b_tmtlqeaa:eb5d00ac-2a4f-4beb-80ad-9da11cff5a70@ds027908.mongolab.com:27908/cloudfoundry-test_2p6otl8c_841b7q4b"
+
+
 var url = 'mongodb://doug:doug@ds037175.mongolab.com:37175/CloudFoundry_l1hnqf0n_mmhir01l';
 mongoose.connect(url);
 console.log("In db.js, mongoose connecting");
