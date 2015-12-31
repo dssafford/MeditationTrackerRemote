4bafaf49791b6b23b9237a9b96b569ab49a1fcbd
Doug Safford <doug.safford@yahoo.com>
2015-12-31 13:20:39 -0600

    modified db.js and CF deploys fine, using env variables

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
-
+var url = '';
 //mongoose.connect('mongodb://doug:doug@ds049651.mongolab.com:49651/flights');
+
+
+if(null == process.env.VCAP_SERVICES) {
+	url = 'mongodb://localhost:27017/DougStudy';
+}
+else {
+	var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+	url = vcap_services['mongolab'][0].credentials.uri;
+}
 
 //var url = 'mongodb://localhost:27017/DougStudy';
 
@@ -9,8 +18,7 @@ var mongoose = require('mongoose');
 //uri: "mongodb://cloudfoundry-test_2p6otl8c_841b7q4b_tmtlqeaa:eb5d00ac-2a4f-4beb-80ad-9da11cff5a70@ds027908.mongolab.com:27908/cloudfoundry-test_2p6otl8c_841b7q4b"
 
 
-var url = 'mongodb://doug:doug@ds037175.mongolab.com:37175/CloudFoundry_l1hnqf0n_mmhir01l';
->>>>>>> cfdeploymentchanges
+//var url = 'mongodb://doug:doug@ds037175.mongolab.com:37175/CloudFoundry_l1hnqf0n_mmhir01l';
 mongoose.connect(url);
 console.log("In db.js, mongoose connecting");
 
