65bd9845693f61315ccd05fe0675cc63488c52d1
Doug Safford <doug.safford@yahoo.com>
2015-12-31 12:19:26 -0600

    cf deploy works.  got db uri that works to mongolab from cf

--- db.js
+++ db.js
@@ -2,7 +2,15 @@ var mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://doug:doug@ds049651.mongolab.com:49651/flights');
 
-var url = 'mongodb://localhost:27017/DougStudy';
+//var url = 'mongodb://localhost:27017/DougStudy';
+
+ //mongodb://<dbuser>:<dbpassword>@ds037175.mongolab.com:37175/CloudFoundry_l1hnqf0n_mmhir01l
+
+//uri: "mongodb://cloudfoundry-test_2p6otl8c_841b7q4b_tmtlqeaa:eb5d00ac-2a4f-4beb-80ad-9da11cff5a70@ds027908.mongolab.com:27908/cloudfoundry-test_2p6otl8c_841b7q4b"
+
+
+var url = 'mongodb://doug:doug@ds037175.mongolab.com:37175/CloudFoundry_l1hnqf0n_mmhir01l';
+>>>>>>> cfdeploymentchanges
 mongoose.connect(url);
 console.log("In db.js, mongoose connecting");
 
