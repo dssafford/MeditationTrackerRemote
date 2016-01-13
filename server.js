d26a4c71b027e1a4a3f76b86933fc8e190f1d0c7
Doug Safford <doug.safford@yahoo.com>
2016-01-12 22:06:12 -0500

    remove token auth in app.js

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http'),
-	entrys = require('./data'),
-	db = require('./db'),
-	app = require('./app')(entrys);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('In Server.js, Doug server listening on port ' + app.get('port'));
-});
